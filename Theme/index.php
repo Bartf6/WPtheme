ab8cd400864c9ce86dd83ab7eb6be1e64d1d7404
W1-FransenBart <bartfransen66@gmail.com>
2016-12-15 15:05:08 +0100

    Begin van theme

---
+++ Theme/index.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title></title>
+        <link href="style.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        
+    </body>
+</html>
